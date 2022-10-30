3e88b928ec2cec0812ab986d65417120d5cca2cb
Timothy Dennis <timothy.dennis@hexagon.com>
2022-10-30 17:56:08 +0200

    add action chain to environment

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 
 def before_all(context):
@@ -8,3 +9,4 @@ def before_all(context):
     context.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=context.options)
     context.driver.maximize_window()
     context.wait = WebDriverWait(context.driver, 10)
+    context.action_chain = ActionChains(context.driver)
