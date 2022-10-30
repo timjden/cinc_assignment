630a4253ba367654fa2b70c8394cda86a4d80565
Timothy Dennis <timothy.dennis@hexagon.com>
2022-10-30 16:50:14 +0200

    add wait for element function

--- features/utils.py
+++ features/utils.py
@@ -1,4 +1,5 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 
-
-
-        
+def wait_for_element_visibility(context, element_xpath):
+    context.wait.until(expected_conditions.visibility_of_element_located((By.XPATH, element_xpath)))
