4f7696345c881b3ba99b443b01500575c4785e43
Timothy Dennis <timothy.dennis@hexagon.com>
2022-10-30 16:50:26 +0200

    test adding a collection

--- features/collections.feature
+++ features/collections.feature
@@ -6,7 +6,10 @@ Feature: Collections
         Given User is on the Dashboard page
         When User clicks Collections tab
         When User adds a new Collection
+        When User enters a title: <title>
+        When User clicks Save
         Then A new Collection is added
+        Then Delete the Collection
         Examples:
-        |username            |password           |
-        |technical_assignment|uhqom-duqmum-tepXe7|
+        |username            |password           |title          |
+        |technical_assignment|uhqom-duqmum-tepXe7|Test Collection|
--- features/steps/collection_steps.py
+++ features/steps/collection_steps.py
@@ -1,9 +1,13 @@
+from multiprocessing.connection import wait
 import time
+import random
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from behave import given, when, then
+
+from utils import wait_for_element_visibility
 
 @given("User is logged in as username: {username}, password: {password}")
 def step_impl(context, username, password):
@@ -19,16 +23,54 @@ def step_impl(context, username, password):
 
 @given("User is on the Dashboard page")
 def step_impl(context):
-    pass
+    dashboard_title_xpath = "/html/body/div/div[2]/div[1]/div/div/header/h1"
+    wait_for_element_visibility(context, dashboard_title_xpath)
+    dashboard_title_element = context.driver.find_element(By.XPATH, dashboard_title_xpath)
+    assert dashboard_title_element.text == "Dashboard"
 
 @when("User clicks Collections tab")
 def step_impl(context):
-    pass
+    collection_tab_xpath = "/html/body/div/div[2]/div[1]/div/div/ul/li[3]"
+    wait_for_element_visibility(context, collection_tab_xpath)
+    collection_tab_element = context.driver.find_element(By.XPATH, collection_tab_xpath)
+    collection_tab_element.click()
 
 @when("User adds a new Collection")
 def step_impl(context):
-    pass
+    add_a_collection_xpath = "/html/body/div/div[2]/div[2]/div/div/div/p/span"
+    wait_for_element_visibility(context, add_a_collection_xpath)
+    add_a_collection_element = context.driver.find_element(By.XPATH, add_a_collection_xpath)
+    add_a_collection_element.click()
+
+@when("User enters a title: {title}")
+def step_impl(context, title):
+    title_xpath = "/html/body/div/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/label/input"
+    wait_for_element_visibility(context, title_xpath)
+    title_element = context.driver.find_element(By.XPATH, title_xpath)
+    context.title_text = f"{title} {random.randint(0, 1000)}"
+    title_element.send_keys(context.title_text)
+
+@when("User clicks Save")
+def step_impl(context):
+    save_xpath = "/html/body/div/div[2]/div[2]/div/div/div[2]/div/div/div[2]/ul[2]/li/button"
+    wait_for_element_visibility(context, save_xpath)
+    save_element = context.driver.find_element(By.XPATH, save_xpath)
+    save_element.click()
 
 @then("A new Collection is added")
 def step_impl(context):
-    pass
+    collection_xpath = "/html/body/div/div[2]/div[2]/div/div/div/div[2]/header/h2"
+    wait_for_element_visibility(context, collection_xpath)
+    collection_element = context.driver.find_element(By.XPATH, collection_xpath)
+    assert collection_element.text == context.title_text
+
+@then("Delete the Collection")
+def step_impl(context):
+    delete_xpath = "/html/body/div/div[2]/div[2]/div/div/div/div[2]/header/div/div/span[2]"
+    yes_delete_xpath = "/html/body/div/div[2]/div[2]/div/div/div[2]/div/div/div/ul[2]/li/button"
+    wait_for_element_visibility(context, delete_xpath)
+    delete_element = context.driver.find_element(By.XPATH, delete_xpath)
+    delete_element.click()
+    wait_for_element_visibility(context, yes_delete_xpath)
+    yes_delete_element = context.driver.find_element(By.XPATH, yes_delete_xpath)
+    yes_delete_element.click()
