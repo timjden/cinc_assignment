5ec5636d42c5e1552d04cd47ce902ab9f170865c
Timothy Dennis <timothy.dennis@hexagon.com>
2022-11-02 20:00:19 +0200

    chrome extra arguments

--- features/environment.py
+++ features/environment.py
@@ -6,6 +6,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 def before_all(context):
     context.options = webdriver.ChromeOptions()
     context.options.add_experimental_option("detach", True)
+    context.options.add_argument("--no-sandbox")
+    context.options.add_argument("--disable-extensions")
+    context.options.add_argument("--headless")
     context.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=context.options)
     context.driver.maximize_window()
     context.wait = WebDriverWait(context.driver, 10)
